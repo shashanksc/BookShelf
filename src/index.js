79b723fcc035174a4e65ed47628b2140fd6fcddb
shashanksc <fvyrtyler@gmail.com>
2023-09-12 02:52:46 +0530

    2nd commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
+import { AppProvider } from './context';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/Home/Home';
+import Shelf from "./pages/Shelf/Shelf";
+import BookList from "./components/BookList/BookList";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "shelf" element = {<Shelf />} />
+          <Route path = "book" element = {<BookList />} />
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/pages/Shelf/Shelf.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react';
+import "./Shelf.css";
+import shelfImg from "../../images/shelf.jpg";
+
+const About = () => {
+  const [localStorageValues, setLocalStorageValues] = useState([]);
+  const remove = (keyToRemove) => {
+    localStorage.removeItem(keyToRemove);
+    // Update the component's state to reflect the removal
+    setLocalStorageValues((prevValues) =>
+      prevValues.filter((item) => item.key !== keyToRemove)
+    );
+  };
+  useEffect(() => {
+    // Function to retrieve and store local storage values
+    const getLocalStorageValues = () => {
+      const values = [];
+      for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const value = localStorage.getItem(key);
+        values.push({ key, value });
+      }
+      return values;
+    };
+
+    // Fetch and store local storage values when the component mounts
+    const values = getLocalStorageValues();
+    setLocalStorageValues(values);
+  }, []);
+  return (
+    <section className='about'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>My Shelf</h2>
+        </div>
+
+        <div className='about-content grid'>
+          <div className='about-img'>
+            <img src = {shelfImg} alt = "" />
+          </div>
+          <div className='about-text'>
+            <h2 className='about-title fs-26 ls-1'>My BookList</h2>
+            <div>
+      
+      <ul>
+        {localStorageValues.map((item) => (
+          <li key={item.key}>
+            
+            <div className="centered-div">{item.value}</div>
+            <div className="centered-div">
+            <div className='removeBtn' onClick={() => remove(item.key)}>Remove</div>
+            </div>
+            
+            
+        
+          </li>
+        ))}
+      </ul>
+    </div>
+
+            </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
