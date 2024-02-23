dfd84b9ec60385ea7b64417d9f7d1d84f92541b3
shashanksc <fvyrtyler@gmail.com>
2024-02-23 22:22:55 +0530

    added icons in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import emailImg from "../../images/email.png"
+import linkedinImg from "../../images/linkedin.png"
+import gitImg from "../../images/github.png"
 import './Footer.css';
 function Footer() {
   return (
     <footer class="footer">
-    <section id="contact">
+    
         <p class="section_text_p1">Get in Touch</p>
-        <h1 class="title">Contact Me</h1>
+        
         <div class="contact-info-upper-container">
             <div class="contact-info-container">
-                <img src=".src\images\email.png" 
+                <img src={emailImg}
                     alt="email icon" 
                     class="icon contact-icon email-icon"/>
                 <p><a href="mailto:fvyrtyler@gmail.com">fvyrtyler@gmail.com</a></p>
             </div>
             <div class="contact-info-container">
-                <img src="src\images\linkedin.png" alt="linkedin icon" class="icon contact-icon"/>
+                <img src={linkedinImg} alt="linkedin icon" class="icon contact-icon"/>
                 <p><a href="https://www.linkedin.com/in/shashank-saurabh-a42816190/" target="_blank">LinkedIn</a></p>
             </div>
+            <div class="contact-info-container">
+                <img src={gitImg} alt="github icon" class="icon contact-icon"></img>
+                <a href="https://github.com/shashanksc" target="_blank">Github</a>
+            </div>
         </div>
-    </section>
+   
 </footer>
 
   );
