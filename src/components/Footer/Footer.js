7e68c3579c654d8ddb9e5b1ad2c95ffa69fc431b
shashanksc <fvyrtyler@gmail.com>
2024-02-23 23:35:32 +0530

    some fixing

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,17 +11,17 @@ function Footer() {
         
         <div class="contact-info-upper-container">
             <div class="contact-info-container">
-                <img src={emailImg}
+                {/* <img src={emailImg}
                     alt="email icon" 
-                    class="icon contact-icon email-icon"/>
+                    class="icon contact-icon email-icon"/> */}
                 <p><a href="mailto:fvyrtyler@gmail.com">fvyrtyler@gmail.com</a></p>
             </div>
             <div class="contact-info-container">
-                <img src={linkedinImg} alt="linkedin icon" class="icon contact-icon"/>
+                {/* <img src={linkedinImg} alt="linkedin icon" class="icon contact-icon"/> */}
                 <p><a href="https://www.linkedin.com/in/shashank-saurabh-a42816190/" target="_blank">LinkedIn</a></p>
             </div>
             <div class="contact-info-container">
-                <img src={gitImg} alt="github icon" class="icon contact-icon"></img>
+                {/* <img src={gitImg} alt="github icon" class="icon contact-icon"></img> */}
                 <a href="https://github.com/shashanksc" target="_blank">Github</a>
             </div>
         </div>
