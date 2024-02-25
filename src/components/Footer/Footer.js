ebd9c58cf1e98bdc46502a4f34f591423c0696ae
shashanksc <fvyrtyler@gmail.com>
2024-02-25 22:18:42 +0530

    fixing css

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,34 +6,35 @@ import './Footer.css';
 function Footer() {
   return (
     <footer class="footer">
-    
         <p class="section_text_p1">Get in Touch</p>
-        
         <div class="contact-info-upper-container">
-        <div class="contact-info-container">
-    <a href="mailto:fvyrtyler@gmail.com">
-        <img src={emailImg} alt="email icon" style={{width: '2.5rem'}} className="icon contact-icon email-icon" />
-    </a>
-</div>
-<div class="contact-info-container">
-    <a href="https://www.linkedin.com/in/shashank-saurabh-a42816190/" target="_blank">
-        <img src={linkedinImg} alt="linkedin icon" className="icon contact-icon" />
-    </a>
-</div>
-<div class="contact-info-container">
-    <a href="https://github.com/shashanksc" target="_blank">
-        <img src={gitImg} alt="github icon" className="icon contact-icon" />
-    </a>
-</div>
-</div>
-<p className="section_text_p1">Other Projects</p>
-    <div className="other-projects-container">
-        <a href="URL_OF_YOUR_OTHER_PROJECT_1" target="_blank">Project 1</a>
-        <a href="URL_OF_YOUR_OTHER_PROJECT_2" target="_blank">Project 2</a>
-    </div>
-</footer>
-
-  );
+            <div class="contact-info-container">
+                <a href="mailto:fvyrtyler@gmail.com">
+                    <img src={emailImg} alt="email icon" style={{width: '2.5rem'}} className="icon contact-icon email-icon" />
+                </a>
+            </div>
+            <div class="contact-info-container">
+                <a href="https://www.linkedin.com/in/shashank-saurabh-a42816190/" target="_blank">
+                    <img src={linkedinImg} alt="linkedin icon" className="icon contact-icon" />
+                </a>
+            </div>
+            <div class="contact-info-container">
+                <a href="https://github.com/shashanksc" target="_blank">
+                    <img src={gitImg} alt="github icon" className="icon contact-icon" />
+                </a>
+            </div>
+        </div>
+        <p className="section_text_p1">Other Projects</p>
+        <div className="contact-info-upper-container">
+            <div className="contact-info-container">
+                <a href="URL_OF_YOUR_OTHER_PROJECT_1" target="_blank">Poll App</a>
+            </div>
+            <div className="contact-info-container">
+                <a href="URL_OF_YOUR_OTHER_PROJECT_2" target="_blank">Curreny Converter</a>
+            </div>
+        </div>
+    </footer>
+    );
 }
 
 export default Footer;
