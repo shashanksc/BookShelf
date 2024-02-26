8563ee3194061bb0dc6b68e80f6ecbf62c1af5b2
shashanksc <fvyrtyler@gmail.com>
2024-02-26 10:55:35 +0530

    warning fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,20 +5,20 @@ import gitImg from "../../images/github.png"
 import './Footer.css';
 function Footer() {
   return (
-    <footer class="footer">
-        <p class="section_text_p1">Get in Touch</p>
-        <div class="contact-info-upper-container">
-            <div class="contact-info-container">
+    <footer className="footer">
+        <p className="section_text_p1">Get in Touch</p>
+        <div className="contact-info-upper-container">
+            <div className="contact-info-container">
                 <a href="mailto:fvyrtyler@gmail.com">
                     <img src={emailImg} alt="email icon" style={{width: '2.5rem'}} className="icon contact-icon email-icon" />
                 </a>
             </div>
-            <div class="contact-info-container">
+            <div className="contact-info-container">
                 <a href="https://www.linkedin.com/in/shashank-saurabh-a42816190/" target="_blank"rel="noreferrer">
                     <img src={linkedinImg} alt="linkedin icon" className="icon contact-icon" />
                 </a>
             </div>
-            <div class="contact-info-container">
+            <div className="contact-info-container">
                 <a href="https://github.com/shashanksc" target="_blank"rel="noreferrer">
                     <img src={gitImg} alt="github icon" className="icon contact-icon" />
                 </a>
