c937872a67415fd1944ad243182c670816618515
shashanksc <fvyrtyler@gmail.com>
2024-03-01 00:46:23 +0530

    improved cover Image and add button

--- src/components/BookList/Book.jsx
+++ src/components/BookList/Book.jsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { Link } from 'react-router-dom';
-import Modal from '../UI/Modal/Modal';
+
 import './BookList.css';
 import Loader from '../Loader/Loader';
 import defaultCoverImg from "../../images/cover_not_found.jpg";
 
 const Book = ({ book, onBookAdded }) => {
-  const [showModal, setShowModal] = useState(false);
-  const [loadingCover, setLoadingCover] = useState(true);
+   const [loadingCover, setLoadingCover] = useState(true);
 
   const fetchCoverImage = useCallback(async () => {
     try {
@@ -40,17 +39,15 @@ const Book = ({ book, onBookAdded }) => {
     }
   }, [book, fetchCoverImage]);
 
-  const closeModal = () => {
-    setShowModal(false);
-  };
+  
 
   const handleAddBook = () => {
     onBookAdded(book);
-    setShowModal(true);
+    
   };
 
   return (
-    <div className="book-item flex flex-column flex-sb">
+    <div className="book-item flex flex-column flex-sb" >
       <div className="book-item-img">
         {loadingCover ? (
           <Loader/>
@@ -58,34 +55,31 @@ const Book = ({ book, onBookAdded }) => {
           <img src={book.cover_img} alt="cover" />
         )}
       </div>
-      <div className="book-item-info text-center">
-        <Link to={`/book/${book.id}`} {...book}>
-          <div className="book-item-info-item title fw-7 fs-18">
-            <span>{book.title}</span>
+      <div className="newDiv" style={{ background: `url(${book.cover_img})center center`, backgroundSize: 'cover' }}>
+        <div className="book-item-info text-center" >
+          <Link to={`/book/${book.id}`} {...book}>
+            <div className="book-item-info-item title fw-7 fs-18">
+              <span>{book.title}</span>
+            </div>
+          </Link>
+          <div className="book-item-info-item edition-count fs-15">
+            <span className="text-capitalize fw-7">Total Editions: </span>
+            <span>{book.edition_count}</span>
           </div>
-        </Link>
-        <div className="book-item-info-item edition-count fs-15">
-          <span className="text-capitalize fw-7">Total Editions: </span>
-          <span>{book.edition_count}</span>
-        </div>
-        <div className="book-item-info-item publish-year fs-15">
-          <span className="text-capitalize fw-7">First Publish Year: </span>
-          <span>{book.first_publish_year}</span>
-        </div>
-      </div>
-      <div>
+          <div className="book-item-info-item publish-year fs-15">
+            <span className="text-capitalize fw-7">First Publish Year: </span>
+            <span>{book.first_publish_year}</span>
+          </div>
+        <div>
         {!localStorage.getItem(book.id) && (
           <div className="addBtn" onClick={handleAddBook}>
-            add to shelf
+            Add
           </div>
         )}
       </div>
-      {showModal && (
-        <Modal show={showModal} modalClosed={closeModal}>
-          <h2>{book.title}</h2>
-          <p>{book.title} has been added to your shelf.</p>
-        </Modal>
-      )}
+      </div>
+      </div>
+      
     </div>
   );
 };
